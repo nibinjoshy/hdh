44d2db0834a6feb926fc0e4753dbe737fe248782
nibinjoshy <66796497+nibinjoshy@users.noreply.github.com>
2020-08-06 19:57:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"nibin.j1721@saintgits.org\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
